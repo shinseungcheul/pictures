263d05473c9aa342f566d42be40350aae93db3e2
SeungChul Shin <s.seungcheul@gmail.com>
2018-03-21 12:45:50 +0900

    templete.co 탬플릿 추가 & 수정중

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -10,9 +10,101 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                spring - react.js
-            </div>
+			<div>
+
+          		<section id="banner" data-video="images/banner">
+  					<div class="inner">
+  						<header>
+  							<h1>Full Motion</h1>
+  							<p>A responsive video gallery template with a functional lightbox<br />
+  							designed by <a href="https://templated.co/">Templated</a> and released under the Creative Commons License.</p>
+  						</header>
+  						<a href="#main" class="more">Learn More</a>
+  					</div>
+  				</section>
+
+
+  				<div id="main">
+  					<div class="inner">
+
+
+  						<div class="thumbnails">
+
+  							<div class="box">
+  								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic01.jpg" alt="" /></a>
+  								<div class="inner">
+  									<h3>Nascetur nunc varius commodo</h3>
+  									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+  									<a href="https://youtu.be/s6zR2T9vn2c" class="button fit" data-poptrox="youtube,800x400">Watch</a>
+  								</div>
+  							</div>
+
+  							<div class="box">
+  								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+  								<div class="inner">
+  									<h3>Nascetur nunc varius commodo</h3>
+  									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+  									<a href="https://youtu.be/s6zR2T9vn2c" class="button style2 fit" data-poptrox="youtube,800x400">Watch</a>
+  								</div>
+  							</div>
+
+  							<div class="box">
+  								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+  								<div class="inner">
+  									<h3>Nascetur nunc varius commodo</h3>
+  									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+  									<a href="https://youtu.be/s6zR2T9vn2c" class="button style3 fit" data-poptrox="youtube,800x400">Watch</a>
+  								</div>
+  							</div>
+
+  							<div class="box">
+  								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
+  								<div class="inner">
+  									<h3>Nascetur nunc varius commodo</h3>
+  									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+  									<a href="https://youtu.be/s6zR2T9vn2c" class="button style2 fit" data-poptrox="youtube,800x400">Watch</a>
+  								</div>
+  							</div>
+
+  							<div class="box">
+  								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
+  								<div class="inner">
+  									<h3>Nascetur nunc varius commodo</h3>
+  									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+  									<a href="https://youtu.be/s6zR2T9vn2c" class="button style3 fit" data-poptrox="youtube,800x400">Watch</a>
+  								</div>
+  							</div>
+
+  							<div class="box">
+  								<a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
+  								<div class="inner">
+  									<h3>Nascetur nunc varius commodo</h3>
+  									<p>Interdum amet accumsan placerat commodo ut amet aliquam blandit nunc tempor lobortis nunc non. Mi accumsan.</p>
+  									<a href="https://youtu.be/s6zR2T9vn2c" class="button fit" data-poptrox="youtube,800x400">Watch</a>
+  								</div>
+  							</div>
+
+  						</div>
+
+  					</div>
+  				</div>
+
+
+  				<footer id="footer">
+  					<div class="inner">
+  						<h2>Etiam veroeros lorem</h2>
+  						<p>Pellentesque eleifend malesuada efficitur. Curabitur volutpat dui mi, ac imperdiet dolor tincidunt nec. Ut erat lectus, dictum sit amet lectus a, aliquet rutrum mauris. Etiam nec lectus hendrerit, consectetur risus viverra, iaculis orci. Phasellus eu nibh ut mi luctus auctor. Donec sit amet dolor in diam feugiat venenatis. </p>
+
+  						<ul class="icons">
+  							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+  							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+  							<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+  							<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
+  						</ul>
+  						<p class="copyright">&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com/">Unsplash</a>. Videos: <a href="http://coverr.co/">Coverr</a>.</p>
+  					</div>
+  				</footer>
+			</div>
         )
     }
 }
